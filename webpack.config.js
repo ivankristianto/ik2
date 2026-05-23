7e75454b2e4f1f443b5d09862e5e1860db60d12a
Ivan K <git@ik2.org>
2026-05-23 13:37:09 +0700

    build: merge default block entries into theme webpack config
    So viewScriptModule bundles for theme blocks (articles-filters, cmd-palette)
    are produced alongside the existing index/editor entries.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require( 'path' );
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
+const defaultEntries =
+	typeof defaultConfig.entry === 'function'
+		? defaultConfig.entry()
+		: defaultConfig.entry || {};
+
 module.exports = {
 	...defaultConfig,
 	entry: {
+		...defaultEntries,
 		index: path.resolve( __dirname, 'wp-content/themes/ik2/src/index.js' ),
 		editor: path.resolve( __dirname, 'wp-content/themes/ik2/src/editor.js' ),
 	},
